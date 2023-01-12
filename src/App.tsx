05d80798958919e2c840c3315f3983b392b37938
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-12 17:04:57 +0200

    Added a Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the maintenance!</h1>
-    </div>
+    <Navbar/>
   );
 }
 
