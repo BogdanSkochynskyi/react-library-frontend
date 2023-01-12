815ed97ec1192e30f87de43682d74aba22dbbe79
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-12 17:32:49 +0200

    Add explore top books component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { ExploteTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <ExploteTopBooks/>
+    </div>
+    
   );
 }
 
