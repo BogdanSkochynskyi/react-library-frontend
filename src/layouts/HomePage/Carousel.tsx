c130a9c5c947de456ea3e6f4c59dfac65f064a26
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-12 17:59:19 +0200

    Return book component added

--- src/layouts/HomePage/Carousel.tsx
+++ src/layouts/HomePage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
         <div className="container mt-5" style={{ height: 550 }}>
@@ -10,50 +12,23 @@ export const Carousel = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151'
-                                        height='233'
-                                        alt="book"
-                                    />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Luv2code</p>
-                                    <a className="btn main-color text-white" href="#">Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook/>
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151'
-                                        height='233'
-                                        alt="book"
-                                    />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Luv2code</p>
-                                    <a className="btn main-color text-white" href="#">Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook/>
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151'
-                                        height='233'
-                                        alt="book"
-                                    />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Luv2code</p>
-                                    <a className="btn main-color text-white" href="#">Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook/>
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
                     </div>
                 </div>
@@ -73,16 +48,7 @@ export const Carousel = () => {
             {/* Mobile */}
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-content-center">
-                    <div className="text-center">
-                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')}
-                            width='151'
-                            height='233'
-                            alt="book"
-                        />
-                        <h6 className="mt-2"><b>Book</b></h6>
-                        <p>Luv2code</p>
-                        <a className="btn main-color text-white" href="#">Reserve</a>
-                    </div>
+                    <ReturnBook/>
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
