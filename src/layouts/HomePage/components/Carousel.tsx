f0201b946176867e765e5c908b0dea23e6cd6a0f
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-17 14:08:40 +0200

    Added a props to carousel books

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -70,23 +70,23 @@ export const Carousel = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <ReturnBook/>
-                            <ReturnBook/>
-                            <ReturnBook/>
+                            {books.slice(0, 3).map(book => (
+                                <ReturnBook book={book} key = {book.id}/>
+                            ))}
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <ReturnBook/>
-                            <ReturnBook/>
-                            <ReturnBook/>
+                            {books.slice(3, 6).map(book => (
+                                <ReturnBook book={book} key = {book.id}/>
+                            ))} 
                         </div>
                     </div>
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <ReturnBook/>
-                            <ReturnBook/>
-                            <ReturnBook/>
+                            {books.slice(6, 9).map(book => (
+                                <ReturnBook book={book} key = {book.id}/>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -106,7 +106,7 @@ export const Carousel = () => {
             {/* Mobile */}
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-content-center">
-                    <ReturnBook/>
+                    <ReturnBook book={books[7]} key = {books[7].id}/>
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
