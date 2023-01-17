e285b4932a115b7a4bd1330e433af8abf78d44e6
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-17 14:14:27 +0200

    Added a loading spinner

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Carousel = () => {
 
@@ -45,16 +46,14 @@ export const Carousel = () => {
 
     if(isLoading) {
         return (
-            <div className="container m-5">
-                <p>Loading...</p>
-            </div>
+            <SpinnerLoading/>
         )
     }
 
     if(httpError) {
         return (
             <div className="container m-5">
-                <p>{httpError}</p>
+                <p className="text-danger">{httpError}</p>
             </div>
         )
     }
