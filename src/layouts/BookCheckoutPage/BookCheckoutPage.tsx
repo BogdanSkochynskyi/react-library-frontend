a8a549ef23b1845a8733f95858d490d541a4268e
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-18 17:20:54 +0200

    Added dynamic rating

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -74,7 +74,7 @@ export const BookCheckoutPage = () => {
               <h2>{book?.title}</h2>
               <h5 className="text-primary">{book?.author}</h5>
               <p className="lead">{book?.description}</p>
-              <StarsReview rating={4} size={32}/>
+              <StarsReview rating={4.5} size={32}/>
             </div>
           </div>
         </div>
@@ -98,6 +98,7 @@ export const BookCheckoutPage = () => {
             <h2>{book?.title}</h2>
             <h5 className="text-primary">{book?.author}</h5>
             <p className="lead">{book?.description}</p>
+            <StarsReview rating={4.5} size={32}/>
           </div>
         </div>
         <hr />
