a48da6f69db1e83b974c637c239863bc99058801
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-18 14:39:08 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { SearchBook } from './layouts/SearchBooksPage/components/SearchBook';
 import { SearchBookPage } from './layouts/SearchBooksPage/SearchBookPage';
 
 export const App = () => {
   return (
     <div>
-      <Navbar/>
-      {/* <HomePage/> */}
-      <SearchBookPage />
-      <Footer/>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBookPage />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
-    
   );
 }
