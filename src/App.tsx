db6724a45a39bf7b38d78d5ba5ad865a4dec8aa7
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-19 16:23:12 +0200

    Added Login widget from okta

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBookPage } from './layouts/SearchBooksPage/SearchBookPage';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+
+const oktaAuth = new OktaAuth(oktaConfig)
 
 export const App = () => {
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  }
+
+  const history = useHistory();
+
+  const restoreOriginalUrl = async (_oktaAuth:any, originalUrl: any) => {
+      history.replace(toRelativeUrl(originalUrl || '/', window.location.origin));
+  }
+
+
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Navbar />
---
+++ src/Auth/LoginWiget.jsx
@@ -0,0 +1,27 @@
+import { Redirect } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../layouts/Utils/SpinnerLoading";
+import OktaSignInWidget from "./OktaSignInWidjet";
+
+const LoginWidget = ({config}) => {
+    const { oktaAuth, authState } = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    }
+    const onError = (err) => {
+        console.log('Sign in error: ', err);
+    }
+
+    if(!authState) {
+        return (
+        <SpinnerLoading/>
+        );
+    }
+
+    return authState.isAuthenticated ? 
+    <Redirect to={{ pathname: "/"}}/>
+    :
+    <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>
+};
+
+export default LoginWidget;
