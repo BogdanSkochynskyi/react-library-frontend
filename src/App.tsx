d5a4ac1f9f9947808e27ceac6c1a42e3f726b458
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-19 16:45:19 +0200

    Fix bug and add security routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBookPage } from './layouts/SearchBooksPage/SearchBookPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
+import { Security, LoginCallback } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWiget';
 
 const oktaAuth = new OktaAuth(oktaConfig)
 
@@ -19,31 +21,35 @@ export const App = () => {
 
   const history = useHistory();
 
-  const restoreOriginalUrl = async (_oktaAuth:any, originalUrl: any) => {
-      history.replace(toRelativeUrl(originalUrl || '/', window.location.origin));
+  const restoreOriginalUrl = async (_oktaAuth: any, originalUrl: any) => {
+    history.replace(toRelativeUrl(originalUrl || '/', window.location.origin));
   }
 
 
   return (
     <div className='d-flex flex-column min-vh-100'>
-      <Navbar />
-      <div className='flex-grow-1'>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home">
-          <HomePage />
-        </Route>
-        <Route path="/search">
-          <SearchBookPage />
-        </Route>
-        <Route path="/checkout/:bookId">
-          <BookCheckoutPage />
-        </Route>
-      </Switch>
-      </div>
-      <Footer />
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUrl} onAuthRequired={customAuthHandler}>
+        <Navbar />
+        <div className='flex-grow-1'>
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path="/search">
+              <SearchBookPage />
+            </Route>
+            <Route path="/checkout/:bookId">
+              <BookCheckoutPage />
+            </Route>
+            <Route path="/login" render={() => <LoginWidget config={oktaConfig}/>}/>
+            <Route path="/login/callback" component={LoginCallback}/>
+          </Switch>
+        </div>
+        <Footer />
+      </Security>
     </div>
   );
 }
--- src/Auth/OktaSignInWidjet.jsx
+++ src/Auth/OktaSignInWidjet.jsx
@@ -3,29 +3,29 @@ import OktaSignIn from "@okta/okta-signin-widget";
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 import { oktaConfig } from "../lib/oktaConfig";
 
-const OktaSignInWidget = ({ onSuccess, onError}) => {
-    const widgetRef = useRef;
+const OktaSignInWidget = ({ onSuccess, onError }) => {
+    const widgetRef = useRef();
 
-    useEffect (() => {
+    useEffect(() => {
 
-        if(!widgetRef.current) {
+        if (!widgetRef.current) {
             return false;
         }
 
         const widget = new OktaSignIn(oktaConfig);
 
         widget.showSignInToGetTokens({
-            el: widget.current,
+            el: widgetRef.current,
         }).then(onSuccess).catch(onError);
 
         return () => widget.remove();
     }, [onSuccess, onError]);
 
     return (
-        <div className="container mt-5 mb-5">
+        <div className='container mt-5 mb-5'>
             <div ref={widgetRef}></div>
-        </div>   
+        </div>
     );
 };
 
-export default OktaSignInWidget;
+export default OktaSignInWidget
