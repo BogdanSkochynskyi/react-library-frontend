4bea535e7d8c0914f45d0163a3a7d51d0396a1af
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-01-25 13:42:46 +0200

    Added admin to navbar

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -42,6 +42,11 @@ export const Navbar = () => {
                 <NavLink className='nav-link' to='/messages'>Messages</NavLink>
               </li>
             }
+            {authState.isAuthenticated && authState.accessToken?.claims?.userType === 'admin' && 
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='/admin'>Admin</NavLink>
+              </li>
+            }
           </ul>
           <ul className='navbar-nav ms-auto'>
             {!authState.isAuthenticated ?
